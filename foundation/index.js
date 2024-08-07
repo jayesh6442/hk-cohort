61f126186b94be97687f36673e7c58ec2cfb5a05
jayesh <jayeshsarvaiya9913@gamil.com>
2024-08-08 00:34:19 +0530

    add code

---
+++ foundation/index.js
@@ -0,0 +1,8 @@
+// console.log("we are in the range ");
+
+// let a = "jayesh";
+// console.log(a);
+
+console.log("hello world");
+
+
---
+++ foundation/one.cpp
@@ -0,0 +1,9 @@
+// #include <stdio.h>
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   cout << "hello jayesh" << endl;
+//   return 0
+// }
