1e4b0f9aeae2c350de557258659e5f1bed73dedd
jayesh <jayeshsarvaiya9913@gamil.com>
2024-08-08 02:21:57 +0530

    add code

---
+++ foundation/class.js
@@ -0,0 +1,33 @@
+// let person = ["jayesh", "savan", "jasani"];
+
+// function printValue(data) {
+//   console.log(data);
+// }
+
+// person.forEach((value) => {
+//   console.log(value);
+// });
+
+// person.forEach(printValue);
+
+// class Animal {
+//   constructor(name, legCount, speaks) {
+//     this.name = name;
+//     this.legCount = legCount;
+//     this.speaks = speaks;
+//   }
+//   speak() {
+//     console.log(this.name + " speaks " + this.speaks);
+//   }
+// }
+
+// let cat = new Animal("cat", 4, "meow");
+// cat.speak();
+// // console.log(cat.legCount);
+
+// let dog = new Animal("dog", 4, "bhow bhow ");
+// dog.speak()
+
+// loop function callback
+
+
---
+++ foundation/jsapi.js
@@ -0,0 +1,14 @@
+// basic js api line lenght and str sub str etc etc
+
+function getLenght(str) {
+  console.log("original str:", str);
+  console.log("lenght:", str.length);
+}
+
+// getLenght("jayesh");
+
+// get more of this from the assignment
+
+
+
+
