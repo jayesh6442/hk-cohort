1ad9c8fd8d4c6c416f37f6f30a96eb3046abb290
jayesh <jayeshsarvaiya9913@gamil.com>
2024-08-08 00:48:13 +0530

    add foundation

--- foundation/index.js
+++ foundation/index.js
@@ -1,8 +1,32 @@
 // console.log("we are in the range ");
-
 // let a = "jayesh";
 // console.log(a);
+// console.log("hello world");
+// let a = "savan jani";
+// console.log(a);
+// console.log("hello world");
+// console.log(b);
+// js is dynamic writen lang thats why we use the typescripte for the good development
+// a =w "sarvaiya";
+// console.log(a);
+// js is single threded lang thats why the js is slow than other lang
+// it can not run two process at the same time
 
-console.log("hello world");
+// variables
 
+// let a = 1;
+// let b = "sarvaiya";
+// let c = false;
+// let d = [1, 2, 3, 4];
+// let e = {
+//   fname: "jayesh",
+//   lname: "sarvaiya",
+// };
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+
+// we have this kind of the data type in the js language
 
